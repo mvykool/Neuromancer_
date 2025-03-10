c7718ecde99e7a569283fdd776dda378802452a5
Maicol Hernandez <maykolandres4@gmail.com>
2025-03-09 23:41:37 -0400

    added primaty color

--- client/src/components/login/FloatingWindow.tsx
+++ client/src/components/login/FloatingWindow.tsx
@@ -3,9 +3,9 @@ import { strings } from "../../constants/strings";
 const FloatingWindow = () => {
   return (
     <div className="top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 absolute w-full">
-      <div className="border-x-8 relative border-b-8 border-t-[35px] border-green-400 flex flex-col items-center  min-h-[600px] mx-auto w-5/12">
+      <div className="border-x-8 relative border-b-8 border-t-[35px] border-primary flex flex-col items-center  min-h-[600px] mx-auto w-5/12">
         {" "}
-        <h1 className="text-green-400 text-5xl mt-12 z-10">
+        <h1 className="text-primary text-5xl mt-12 z-10">
           {strings.login.title}
         </h1>
         <img
@@ -15,7 +15,7 @@ const FloatingWindow = () => {
         />
         <button
           type="button"
-          className="bg-green-400 px-4 w-3/6 mx-auto py-3 absolute bottom-20 tracking-wide cursor-pointer"
+          className="bg-primary px-4 w-3/6 mx-auto py-3 absolute bottom-20 tracking-wide cursor-pointer"
         >
           {strings.login.btn}
         </button>
