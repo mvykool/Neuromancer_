79342b9f696533379a0e8cb004470c0ae4234189
Maicol Hernandez <maykolandres4@gmail.com>
2025-03-09 23:47:06 -0400

    temporary link btn

--- client/src/components/login/FloatingWindow.tsx
+++ client/src/components/login/FloatingWindow.tsx
@@ -1,9 +1,14 @@
 import { strings } from "../../constants/strings";
+import { useNavigate } from "react-router-dom";
 
 const FloatingWindow = () => {
+  const navigate = useNavigate();
+
+  const goHome = () => navigate("/");
+
   return (
     <div className="top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 absolute w-full">
-      <div className="border-x-8 relative border-b-8 border-t-[35px] border-primary flex flex-col items-center  min-h-[600px] mx-auto w-5/12">
+      <div className="border-x-8 relative border-b-8 border-t-[35px] border-primary flex flex-col items-center  min-h-[610px] mx-auto w-5/12">
         {" "}
         <h1 className="text-primary text-5xl mt-12 z-10">
           {strings.login.title}
@@ -11,11 +16,12 @@ const FloatingWindow = () => {
         <img
           src="/logoone.png"
           alt="logo"
-          className="object-cover -mt-2 size-[420px]"
+          className="object-cover -mt-2 size-[380px] mr-[4%]"
         />
         <button
+          onClick={goHome}
           type="button"
-          className="bg-primary px-4 w-3/6 mx-auto py-3 absolute bottom-20 tracking-wide cursor-pointer"
+          className="bg-primary px-4 w-3/6 mx-auto py-2 absolute bottom-24 tracking-wide cursor-pointer"
         >
           {strings.login.btn}
         </button>
