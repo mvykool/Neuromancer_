4364a8b4f6367e2ee9d0b3d0012210ee8a2d773b
Maicol Hernandez <maykolandres4@gmail.com>
2025-03-09 22:09:40 -0400

    added orbitron font famility to only certain tags

---
+++ client/src/components/login/FloatingWindow.tsx
@@ -0,0 +1,13 @@
+const FloatingWindow = () => {
+  return (
+    <div className="top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 absolute w-full">
+      <div className="border-x-8 border-b-8 border-t-[35px] border-green-400 flex min-h-[650px] mx-auto w-3/6">
+        {" "}
+        <img src="" alt="" />
+        <button></button>
+      </div>
+    </div>
+  );
+};
+
+export default FloatingWindow;
--- client/src/views/LoginView.tsx
+++ client/src/views/LoginView.tsx
@@ -1,9 +1,11 @@
 import CommandsRain from "../components/login/CommandsRain";
+import FloatingWindow from "../components/login/FloatingWindow";
 
 const LoginView = () => {
   return (
     <div className="p-1 bg-black h-screen">
       <CommandsRain />
+      <FloatingWindow />
     </div>
   );
 };
