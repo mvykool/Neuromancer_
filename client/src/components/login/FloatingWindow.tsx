01cb9f512015cdf362599a493bce80336e3d155e
Maicol Hernandez <maykolandres4@gmail.com>
2025-03-09 22:13:34 -0400

    created strings, and added title

--- client/src/components/login/FloatingWindow.tsx
+++ client/src/components/login/FloatingWindow.tsx
@@ -1,9 +1,12 @@
+import { strings } from "../../constants/strings";
+
 const FloatingWindow = () => {
   return (
     <div className="top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 absolute w-full">
-      <div className="border-x-8 border-b-8 border-t-[35px] border-green-400 flex min-h-[650px] mx-auto w-3/6">
+      <div className="border-x-8 border-b-8 border-t-[35px] border-green-400 flex flex-col items-center  min-h-[650px] mx-auto w-3/6">
         {" "}
-        <img src="" alt="" />
+        <h1 className="text-green-400 text-5xl mt-10">{strings.login.title}</h1>
+        <img src="" alt="logo" />
         <button></button>
       </div>
     </div>
---
+++ client/src/constants/strings.ts
@@ -0,0 +1,5 @@
+export const strings = {
+  login: {
+    title: "Neuromancer_",
+  },
+};
