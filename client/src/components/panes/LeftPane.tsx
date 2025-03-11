ea872f86a540b417cfa20db1808bf6acd5423801
Maicol Hernandez <maykolandres4@gmail.com>
2025-03-11 01:09:27 -0400

    Added default wave radar

--- client/src/components/panes/LeftPane.tsx
+++ client/src/components/panes/LeftPane.tsx
@@ -7,20 +7,28 @@ interface LeftPaneProps {
 
 const LeftPane = ({ name, picture }: LeftPaneProps) => {
   return (
-    <div className="gap-3 flex flex-col border border-red-500 w-4/12">
+    <div className="gap-3 relative flex flex-col w-4/12">
       {/* Radar */}
       <div className="py-5 w-full bg-primary  flex justify-center items-center">
         {" "}
         <p className="text-lg">{strings.home.leftPane.id}</p>
       </div>
       {/* img */}
-      <img src={picture} alt={strings.home.leftPane.altPic} />
+      <img
+        src={picture}
+        alt={strings.home.leftPane.altPic}
+        className="min-h-60 w-full object-cover"
+      />
       {/* Name */}
       <div className="border-4 border-primary w-full py-2 flex justify-center items-center">
         <p className="text-primary tracking-wider text-lg">{`@${name}`}</p>
       </div>
-      j{/* Radar */}
-      <div>radar</div>
+      {/* Radar */}
+      <div className="h-60 w-full overflow-hidden">
+        <video className=" w-full h-full object-cover" autoPlay muted loop>
+          <source src="waveradiant.mp4" type="video/mp4" />
+        </video>
+      </div>
     </div>
   );
 };
